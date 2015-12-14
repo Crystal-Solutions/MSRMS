fae9fbf990d9c61edbf543935fb694e8a4feef54
janaka <bjchathuranga@gmail.com>
2015-12-14 22:39:50 +0530

    Added the faculty entity

---
+++ src/AppBundle/Entity/Faculty.php
@@ -0,0 +1,60 @@
+<?php 
+namespace AppBundle\Entity;
+
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="faculty")
+ */
+class Faculty
+{
+	
+	 /**
+     * @ORM\Column(type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="decimal", scale=2)
+     */
+    protected $name;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Faculty
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
