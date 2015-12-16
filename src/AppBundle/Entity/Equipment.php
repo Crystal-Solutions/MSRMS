5758b8fb50deda738660bf262f3a86bcf29251f7
Ravi <ravindu.hasantha@gmail.com>
2015-12-17 00:26:51 +0530

    id equipment

--- src/AppBundle/Entity/Equipment.php
+++ src/AppBundle/Entity/Equipment.php
@@ -78,11 +78,11 @@ class Equipment
         }
 
         $equipment = new Equipment();
-        $stmt = $con->prepare('SELECT name,description,amount FROM equipment WHERE equipment.id=?');
+        $stmt = $con->prepare('SELECT id,name,description,amount FROM equipment WHERE equipment.id=?');
         $stmt->bind_param("s",$id);
         $stmt->execute();
 
-        $stmt->bind_result($equipment->name,$equipment->description,$equipment->amount);
+        $stmt->bind_result($equipment->id,$equipment->name,$equipment->description,$equipment->amount);
         $stmt->fetch();
         $stmt->close();
         return $equipment;
@@ -97,12 +97,13 @@ class Equipment
         }
 
          $equipments = array(); //Make an empty array
-        $stmt = $con->prepare('SELECT name,description,amount FROM equipment');
+        $stmt = $con->prepare('SELECT id,name,description,amount FROM equipment');
         $stmt->execute();
-        $stmt->bind_result($name,$description,$amount);
+        $stmt->bind_result($id,$name,$description,$amount);
         while($stmt->fetch())
         {
             $equipment = new Equipment();
+            $equipment->id=$id;
             $equipment->setName($name);
             $equipment->setDescription($description);
             $equipment->setAmount($amount);
