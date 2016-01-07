43cb2b71d1c5553fc4cd6f468d59aef410f5e327
Ravi <ravindu.hasantha@gmail.com>
2016-01-07 20:58:20 +0530

    equipmentborrowedbyplayer added

--- src/AppBundle/Entity/EquipmentBorrowedByPlayer.php
+++ src/AppBundle/Entity/EquipmentBorrowedByPlayer.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-
+use AppBundle\Controller\Connection;
 /**
  * EquipmentBorrowedByPlayer
  *
@@ -76,7 +76,10 @@ class EquipmentBorrowedByPlayer
      */
     private $equipment;
 
- public function save()
+    public $equipment_id;
+    public $player_id;
+
+    public function save()
     {
 
         $this->borrowedTime = $this->borrowedTime->format('Y-m-d H-i-s');
@@ -88,7 +91,7 @@ class EquipmentBorrowedByPlayer
            
         $con = Connection::getConnectionObject()->getConnection();
         $stmt = $con->prepare('INSERT INTO `equipment_borrowed_by_player` (`equipment_id`,`player_id`,`amount`, `borrowed_time`, `due_time`, `returned_time`, `issue_details`) VALUES (?,?,?,?,?,?,?)');  
-        $stmt->bind_param("iiissss",$this->equipment,$this->playerId,$this->amount,$this->borrowedTime,$this->dueTime,$this->returnedTime,$this->issueDetails);  
+        $stmt->bind_param("iiissss",$this->equipment_id,$this->player_id,$this->amount,$this->borrowedTime,$this->dueTime,$this->returnedTime,$this->issueDetails);  
         $stmt->execute();  
         $stmt->close();
         }
@@ -96,7 +99,7 @@ class EquipmentBorrowedByPlayer
         {
         $con = Connection::getConnectionObject()->getConnection();
         $stmt = $con->prepare('UPDATE EquipmentBorrowedByPlayer SET equipment_id =?,player_id=?,amount=?,borrowed_time=?,due_time=?,returned_time=?,issue_details=? WHERE player.id = id');  
-        $stmt->bind_param("iiissss",$this->equipment,$this->playerId,$this->amount,$this->borrowedTime,$this->dueTime,$this->returnedTime,$this->issueDetails);  
+        $stmt->bind_param("iiissss",$this->equipment_id,$this->player_id,$this->amount,$this->borrowedTime,$this->dueTime,$this->returnedTime,$this->issueDetails);  
         $stmt->execute();  
         $stmt->close();   
         }
@@ -112,7 +115,7 @@ class EquipmentBorrowedByPlayer
         }
 
         $equipmentBorrowedByPlayer = new EquipmentBorrowedByPlayer();
-        $stmt = $con->prepare('SELECT id,equipment_id,player_id,amount,borrowed_time,due_time,returned_time,issue_details FROM player WHERE id=?');
+        $stmt = $con->prepare('SELECT id,equipment_id,player_id,amount,borrowed_time,due_time,returned_time,issue_details FROM equipment_borrowed_by_player WHERE id=?');
         $stmt->bind_param("s",$id);
         $stmt->execute();
 
@@ -131,15 +134,16 @@ class EquipmentBorrowedByPlayer
         }
 
         $equipmentBorrowedByPlayers = array(); //Make an empty array
-        $stmt = $con->prepare('SELECT id,equipment_id,player_id,amount,borrowed_time,due_time,returned_time,issue_details FROM equipmentBorrowedByPlayer');
+        $stmt = $con->prepare('SELECT id,equipment_id,player_id,amount,borrowed_time,due_time,returned_time,issue_details FROM equipment_borrowed_by_player');
         $stmt->execute();
-        $stmt->bind_result($id,$equipment,$playerId,$amount,$borrowedTime,$dueTime,$returnedTime,$issueDetails);
+        $stmt->bind_result($id,$equipment_id,$player_id,$amount,$borrowedTime,$dueTime,$returnedTime,$issueDetails);
         while($stmt->fetch())
         {
-            $player = new Player();
-            $player->id=$id;
-           // $equipmentBorrowedByPlayer->setEquipment($equipmentId);
-            $equipmentBorrowedByPlayer->setPlayer($player);
+            $equipmentBorrowedByPlayer = new EquipmentBorrowedByPlayer();
+            $equipmentBorrowedByPlayer->id=$id;
+            //check here k
+            $equipmentBorrowedByPlayer->equipment_id=$equipment_id;
+            $equipmentBorrowedByPlayer->player_id=$player_id;
             $equipmentBorrowedByPlayer->setAmount($amount);
             $equipmentBorrowedByPlayer->setBorrowedTime($borrowedTime);
             $equipmentBorrowedByPlayer->setDueTime($dueTime);
@@ -331,4 +335,17 @@ class EquipmentBorrowedByPlayer
     {
         return $this->equipment;
     }
+/*
+// i added these playerid and equipmentid getters and setters
+        public function setPlayerId($playerId)
+    {
+        $this->playerId = $playerId;
+
+        return $this;
+    }
+
+      public function getPlayerId()
+    {
+        return $this->playerId;
+    }*/
 }
