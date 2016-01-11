457f90e2efc85cedd255a6c7eb8df918706acd21
Ravi <ravindu.hasantha@gmail.com>
2016-01-11 23:55:02 +0530

    usecase: Equipment allocate to sport

---
+++ src/AppBundle/Controller/EquipmentAllocateController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\SportHasEquipment;
+use AppBundle\Entity\Sport;    
+use AppBundle\Entity\Equipment;
+use AppBundle\Entity\AuthorizingOfficer;
+  
+
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+
+
+class EquipmentAllocateController extends Controller
+{
+    /**
+     * @Route("/equipmentAllocate/", name="equipment_allocate_home")
+     */
+    public function indexAction(Request $request)
+    {
+        $sportHasEquipment = new SportHasEquipment(); 
+
+        //Generate required data for the form ----------------------- For choices
+        $sports =  Sport::getAll();
+        $sportIds = array();
+        foreach ($sports as $sport) {
+            $sportIds[$sport->getName()] = $sport->getId();
+        }
+
+        $equipments =  Equipment::getAll();
+        $equipmentIds = array();
+        foreach ($equipments as $eq) {
+            $equipmentIds[$eq->getName()] = $eq->getId();
+        }
+
+        $authorizingOfficers = AuthorizingOfficer :: getAll();
+        $authorizingOfficerIds = array();
+
+        foreach ($authorizingOfficers as $authorizingOfficer){
+            $authorizingOfficerIds[$authorizingOfficer->getName()]= $authorizingOfficer->getId();
+        }
+        //------------------------------------------------------------------------
+
+
+
+        $form = $this->createFormBuilder($sportHasEquipment)
+            ->add('equipment_id',ChoiceType::class, array(
+            'choices'  => $equipmentIds,
+            'choices_as_values' => true,
+            'label'=>'Equipment'
+                ))
+            ->add('sport_id',ChoiceType::class, array(
+            'choices'  => $sportIds,
+            'choices_as_values' => true,
+            'label'=>'Sport'
+                ))   
+            ->add('authorizing_officer_id',ChoiceType::class, array(
+            'choices'  => $authorizingOfficerIds,
+            'choices_as_values' => true,
+            'label'=>'Authorizing Officer'
+                )) 
+
+            ->add('save', SubmitType::class, array('label' => 'Allocate Equipment'))
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // ... perform some action, such as saving the task to the database
+            $sportHasEquipment->save();
+
+            return $this->redirectToRoute('task_success');
+        }
+
+        // replace this example code with whatever you need
+        return $this->render('usecases/equipment_allocate_home.html.twig', array('form' => $form->createView()));
+    }
+
+ 
+}
--- src/AppBundle/Entity/SportHasEquipment.php
+++ src/AppBundle/Entity/SportHasEquipment.php
@@ -3,7 +3,7 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-
+use AppBundle\Controller\Connection;
 /**
  * SportHasEquipment
  *
@@ -50,14 +50,85 @@ class SportHasEquipment
      * })
      */
     private $equipment;
+    private $equipmentId;
+    private $sportId;
+    private $authorizingOfficerId;
+/////////////methods//////
+ public function save()
+    {
+        if ($this->id == null)
+        {
+            $con = Connection::getConnectionObject()->getConnection();
+            $stmt = $con->prepare('INSERT INTO sport_has_equipment (equipment_id,sport_id,authorizing_officer_id) VALUES (?,?,?)');  
+            $stmt->bind_param("iii",$this->equipmentId,$this->sportId,$this->authorizingOfficerId);  
+            $stmt->execute();  
+            $stmt->close();
+        }
+        
+        else
+        {
+            $con = Connection::getConnectionObject()->getConnection();
+            $stmt = $con->prepare('UPDATE sport_has_equipment SET equipment_id = ? , sport_id = ? WHERE authorizing_officer_id= ?');  
+            $stmt->bind_param("iii",$this->euipmentId,$this->sportId,$this->authorizingOfficerId);  
+            $stmt->execute();  
+            $stmt->close();
+        }
+    }
 
+    public static function getOne($id)
+    {
+        $con = Connection::getConnectionObject()->getConnection();
+        // Check connection
+        if (mysqli_connect_errno())
+        {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
 
+        $sportHasEquipment = new SportHasEquipment();
+        $stmt = $con->prepare('SELECT equipment_id,sport_id,authorizing_officer_id FROM sport_has_equipment WHERE id=?');
+        $stmt->bind_param("s",$id);
+        $stmt->execute();
+
+        $stmt->bind_result($sportHasEquipment->equipmentId,$sportHasEquipment->sportId,$sportHasEquipment->authorizingOfficerId);
+        $stmt->fetch();
+        $stmt->close();
+        return $sportHasEquipment;
+    }
+
+    public static function getAll()
+    {
+        $con = Connection::getConnectionObject()->getConnection();
+        // Check connection
+        if (mysqli_connect_errno())
+        {
+            echo "Failed to connect to MySQL: " . mysqli_connect_error();
+        }
+
+        $stmt = $con->prepare('SELECT id,equipment_id,sport_id,authorizing_officer_id FROM sport_has_equipment');
+        $stmt->execute();
+        $stmt->bind_result($id,$equipmentId,$sportId,$authorizingOfficerId);
+        $sportHasEquipments = array();   
+            while ( $stmt->fetch() ) {
+            $sportHasEquipment = new SportHasEquipment();
+            $sportHasEquipment->id=$id;
+            $sportHasEquipment->setEquipmentId($equipmentId);
+            $sportHasEquipment->setSportId($sportId);
+            $sportHasEquipment->setAuthorizingOfficerId($authorizingOfficerId);
+           
+
+            array_push($sportHasEquipments,$sportHasEquipment);
+            }
+            $stmt->close();
+            return $sportHasEquipments;      
+    }
+  
 
     /**
      * Get id
      *
      * @return integer
      */
+
     public function getId()
     {
         return $this->id;
@@ -133,5 +204,46 @@ class SportHasEquipment
     public function getEquipment()
     {
         return $this->equipment;
+    }
+
+// manually added methods from here on
+    public function setEquipmentId($equipmentId)
+    {
+        $this->equipmentId = $equipmentId;
+
+        return $this;
+    }
+
+
+    public function getEquipmentId()
+    {
+        return $this->equipmentId;
+    }
+///////////////////////////////////////////////////
+    public function setSportId($sportId)
+    {
+        $this->sportId = $sportId;
+
+        return $this;
+    }
+
+
+    public function getSportId()
+    {
+        return $this->sportId;
+    }
+
+///////////////////////////////////////////////////
+    public function setAuthorizingOfficerId($authorizingOfficerId)
+    {
+        $this->authorizingOfficerId = $authorizingOfficerId;
+
+        return $this;
+    }
+
+
+    public function getAuthorizingOfficerId()
+    {
+        return $this->authorizingOfficerId;
     }
 }
