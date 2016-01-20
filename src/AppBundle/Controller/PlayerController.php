2da9d2d39d94c24a9999d3b202513390ed6a8d1b
Ravi <ravindu.hasantha@gmail.com>
2016-01-20 17:12:48 +0530

    styles or table, achievemnts view in player

--- src/AppBundle/Controller/PlayerController.php
+++ src/AppBundle/Controller/PlayerController.php
@@ -143,6 +143,8 @@ class PlayerController extends Controller
         $achievements = $player->getPlayerAchievements($id);
         $borrowedEquipments = $player->getBorrowedEquipments($id);
         $reservedEquipments = $player -> getReservedEquipments($id);
+
+
         return $this->render('player/view.html.twig', array('player' =>$player, 'achievements'=>$achievements,'sports'=>$sports, 'borrowedEquipments'=>$borrowedEquipments, 'reservedEquipments'=>$reservedEquipments));  
 
     }
